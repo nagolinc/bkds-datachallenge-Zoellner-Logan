ba7e92aefd0512749606e2b761845aa66aad3ff2
logan <nagolinc@gmail.com>
2016-06-21 07:17:45 -0400

    parsing most of the data I think I will use

--- parseWebpage.py
+++ parseWebpage.py
@@ -1,4 +1,5 @@
 from html.parser import HTMLParser
+import json
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
@@ -93,6 +94,41 @@ class ParseRoom(HTMLParser):
 		HTMLParser.__init__(self)
 		self.mode="NONE"
 		self.price=None
+		self.data={}
+		self.dataTags={"Accomodates=2.2":"ACCOMODATES",
+			"Bathrooms=2.2":"BATHROOMS",
+			"Bed type=2.2":"BED_TYPE",
+			"Bedrooms=2.2":"BEDROOMS",
+			"Beds=2.2":"BEDS",
+			"Property type=2.0.2":"PROPERTY_TYPE",
+			"Room type=2.2":"ROOM_TYPE"}
+		self.currentDataTag="NONE"
+		self.meta=None
+		self.amenities={}
+		self.scores={}
+		
+		#things I definitely still need to parse
+		# number of stars
+		# number of reviews
+		# number of pictures
+		
+		#I actually might be able to pull all of this out of meta...
+		
+		
+	def parseMeta(self):
+		meta=json.loads(self.meta)
+		listing=meta["listing"]
+		#summarize reviews
+		review_details=listing["review_details_interface"]
+		self.scores["num_reviews"]=review_details['review_count']
+		self.scores["overall"]=review_details['review_score']
+		for detail in review_details['review_summary']:
+			self.scores[detail['label']]=detail['value']
+		#grab amenities
+		amenities=listing['listing_amenities']
+		for amenity in amenities:
+			self.amenities[amenity['name']]=amenity['is_present']
+		
 
 	def handle_starttag(self, tag, attrs):
 		#print( "Encountered a start tag:", tag,attrs)
@@ -101,6 +137,16 @@ class ParseRoom(HTMLParser):
 			self.mode="LOOKING_FOR_PRICE0"
 		if self.mode=="LOOKING_FOR_PRICE0" and tag=='span':
 			self.mode="LOOKING_FOR_PRICE1"
+		if "data-reactid" in ad:
+			for matchString,dataTag in self.dataTags.items():
+				if matchString in ad["data-reactid"]:
+					self.mode="LOOKING_FOR_DATA_TAG"
+					self.currentDataTag=dataTag
+					
+		if tag=="meta" and "id" in ad and ad["id"]=="_bootstrap-listing":
+			self.meta=ad["content"]
+			self.parseMeta()
+					
 
 	def handle_endtag(self, tag):
 		#print "Encountered an end tag :", tag
@@ -112,9 +158,12 @@ class ParseRoom(HTMLParser):
 		if self.mode=="LOOKING_FOR_PRICE1" and "$" in data:
 			self.price=float(data[1:])
 			self.mode="NONE"
+		if self.mode=="LOOKING_FOR_DATA_TAG":
+			self.data[self.currentDataTag]=data
+			self.mode="NONE"
 
 
-url2='https://www.airbnb.com/rooms/12836344'
+url2='https://www.airbnb.com/rooms/7734116'
 req = Request(url2, headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
 roomParser = ParseRoom()
